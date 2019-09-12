66db3e146fb8dacf9c03d173374fedd582c5bbed
Mike J Innes <mike.j.innes@gmail.com>
2019-09-12 13:44:11 +0100

    better names

--- src/xla.jl
+++ src/xla.jl
@@ -1,20 +1,20 @@
-const XLAScalar = Union{Float64,Float32,Float16,Int64,Int32,Int16}
+const XScalar = Union{Float64,Float32,Float16,Int64,Int32,Int16}
 
-struct XLAArray{T,N} <: AbstractArray{T,N}
+struct XArray{T,N} <: AbstractArray{T,N}
   buffer::PyObject
 end
 
-function XLAArray(x::Array{<:XLAScalar})
+function XArray(x::Array{<:XScalar})
   buffer = xlaclient.Buffer.from_pyval(x)
-  xla = XLAArray{eltype(x),ndims(x)}(buffer)
+  xla = XArray{eltype(x),ndims(x)}(buffer)
   delete = buffer.delete # work around a segfault on exit
   finalizer(buf -> ispynull(delete) || delete(), xla.buffer)
   return xla
 end
 
-Base.size(x::XLAArray) = x.buffer.shape().dimensions()
-Base.collect(x::XLAArray) = x.buffer.to_py()
-Base.print_array(io::IO, x::XLAArray) = Base.print_array(io, collect(x))
+Base.size(x::XArray) = x.buffer.shape().dimensions()
+Base.collect(x::XArray) = x.buffer.to_py()
+Base.print_array(io::IO, x::XArray) = Base.print_array(io, collect(x))
 
 function backend(platform="cpu")
   xlaclient.get_local_backend(platform)
