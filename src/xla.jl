8c05eb7849e3904ea8f0a8bce43869b1584d0c7b
Mike Innes <mike.j.innes@gmail.com>
2019-09-16 16:08:42 +0100

    mass import simple ops

--- src/xla.jl
+++ src/xla.jl
@@ -67,13 +67,31 @@ xla(x::AbstractArray) = XArray(x)
 
 # Operation Wrappers
 
-struct Neg end
+function run_direct(op, args...)
+  args = xla.(args)
+  b = xlaclient.ComputationBuilder("")
+  arg_ops = [b.ParameterWithShape(Shape(arg)) for arg in args]
+  getproperty(b, op)(arg_ops...)
+  b.Build().Compile().Execute(args) |> XArray
+end
 
-build!(builder, ::Neg, x) = builder.Neg(x)
+for op in :[Neg, Sign, Floor, Ceil, Round, Exp, Log, Expm1, Log1p, Tanh,
+    Sin, Cos, Lgamma, Digamma, Erf, Erfc, ErfInv, Sqrt, Rsqrt, Not].args
+  @eval begin
+    struct $op end
+    build!(builder, ::$op, x) = getproperty(builder, $(Expr(:quote, op)))(x)
+    (::$op)(x) = run_direct($(Expr(:quote, op)), x)
+  end
+end
 
-struct Add end
-
-build!(builder, ::Add, x, y) = builder.Add(x, y)
+for op in :[Atan2, Pow, And, Or, Xor, Add, Sub, Mul, SafeMul, Div, Rem,
+    Max, Min, ShiftLeft, ShiftRightArithmetic, ShiftRightLogical].args
+  @eval begin
+    struct $op end
+    build!(builder, ::$op, x, y) = getproperty(builder, $(Expr(:quote, op)))(x, y)
+    (::$op)(x, y) = run_direct($(Expr(:quote, op)), x, y)
+  end
+end
 
 # IR Builder
 
