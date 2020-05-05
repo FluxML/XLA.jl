f52bf1cb6963ebf8a1998e1b5c58957cd5350c76
Mike J Innes <mike.j.innes@gmail.com>
2020-05-05 16:37:20 +0100

    broadcast with style

--- src/compile/passes.jl
+++ src/compile/passes.jl
@@ -2,11 +2,12 @@ function broadcasts!(ir)
   for (v, st) in ir
     ex = st.expr
     if isexpr(ex, :call) && ex.args[1] == Broadcast.broadcasted
-      f = exprtype(ir, ex.args[2])
-      args = exprtype.((ir,), ex.args[3:end])
+      f = exprtype(ir, ex.args[3])
+      args = exprtype.((ir,), ex.args[4:end])
       args = eltype.(widen.(args))
       func = trace(f, args...)
       func = insert!(ir, v, func)
+      popfirst!(ex.args) # remove ArrayStyle
       ex.args[1] = broadcast
       ex.args[2] = func
     elseif isexpr(ex, :call) && ex.args[1] == mapreduce
