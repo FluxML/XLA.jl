eda9866dbe65866f968fb92675870e72296a7473
Mike J Innes <mike.j.innes@gmail.com>
2020-05-12 20:11:56 +0100

    mnist example

---
+++ examples/mnist.jl
@@ -0,0 +1,26 @@
+using Flux, Optimisers, XLA
+using Flux.Data.MNIST
+using Flux: onehotbatch, crossentropy
+
+images = reduce(hcat, [vec(Float64.(im)) for im in MNIST.images()])
+labels = onehotbatch(MNIST.labels(), 0:9)
+
+loss(m, x, y) = logitcrossentropy(m(x), y)
+
+opt = Optimisers.Descent(1e-3)
+
+function step(m, x, y)
+  m̄ = gradient(m) do m
+    @show crossentropy(m(x), y)
+  end[1]
+  return opt(m, m̄)
+end
+
+x = images[:,1]
+y = Float64.(collect(labels[:,1]))
+
+m = Chain(Dense(28^2, 32, relu), Dense(32, 10), softmax) |> f64
+
+xstep = xla(step)
+
+m = xstep(m, x, y)
