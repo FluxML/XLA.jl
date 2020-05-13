33cffbce55054a3522091d805984fbba17a4f963
Mike J Innes <mike.j.innes@gmail.com>
2020-05-13 11:52:54 +0100

    there were tabs hiding in this file

--- deps/build.jl
+++ deps/build.jl
@@ -5,19 +5,19 @@ JAXLIB="0.1.46"
 if PyCall.conda
   Conda.add("pip")
   pip = joinpath(Conda.BINDIR, "pip")
-	run(`$pip install jaxlib==$JAXLIB`)
+  run(`$pip install jaxlib==$JAXLIB`)
 else
   try
-		pyimport("jaxlib")
-	catch e
-		e isa PyCall.PyError || rethrow(e)
-		error("""
-      Python Dependencies not installed
-      Please either:
-       - Rebuild PyCall to use Conda, by running in the julia REPL:
-         - `using Pkg; ENV["PYTHON"]=""; Pkg.build("PyCall"); Pkg.build("XLA")`
-       - Or install the depencencies, eg by running pip
-      	 - `pip install jaxlib==$JAXLIB`
-    	""")
-	end
+    pyimport("jaxlib")
+  catch e
+    e isa PyCall.PyError || rethrow(e)
+    error("""
+        Python Dependencies not installed
+        Please either:
+         - Rebuild PyCall to use Conda, by running in the julia REPL:
+           - `using Pkg; ENV["PYTHON"]=""; Pkg.build("PyCall"); Pkg.build("XLA")`
+         - Or install the depencencies, eg by running pip
+        	 - `pip install jaxlib==$JAXLIB`
+      	""")
+  end
 end
